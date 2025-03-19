074a49ed70ef622ba069870920dfff48c1051d1f
0xmohitsen <mohitsen0102@gmail.com>
2025-03-20 00:08:54 +0530

    Updated backend url

--- Client/src/Helpers/axiosinstance.js
+++ Client/src/Helpers/axiosinstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  "https://learning-management-system-backend-ihkw.onrender.com/api/v1";
+const BASE_URL = "https://lms-backend-01.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
