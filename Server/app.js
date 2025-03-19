2380bd70e708040d1c527ff44cf1bc438d443cf2
0xmohitsen <mohitsen0102@gmail.com>
2025-03-20 00:02:57 +0530

    Added env var

--- Server/app.js
+++ Server/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://learning-management-system-backend-ihkw.onrender.com",
+    origin: process.env.FRONTEND_URL,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
